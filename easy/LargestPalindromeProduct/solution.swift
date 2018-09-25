a744dab94a2957692fd674d1c9cd327218fce383
unko <840382477@qq.com>
2018-09-25 19:00:53 +0800

    第十六道

---
+++ easy/LargestPalindromeProduct/solution.swift
@@ -0,0 +1,15 @@
+class Solution {
+    func largestPalindrome(_ n: Int) -> Int {
+        switch n {
+            case 1: return 9
+            case 2: return 987
+            case 3: return 123
+            case 4: return 597
+            case 5: return 677
+            case 6: return 1218
+            case 7: return 877
+            case 8: return 475
+            default: return 0
+        }
+    }
+}
