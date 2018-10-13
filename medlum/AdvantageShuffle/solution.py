65cae7ffad8be49cc3156a5f3b9156bd0d98f5c9
unko <840382477@qq.com>
2018-10-13 15:03:42 +0800

    第五十五道

---
+++ medlum/AdvantageShuffle/solution.py
@@ -0,0 +1,22 @@
+class Solution:
+    def advantageCount(self, A, B):
+        """
+        :type A: List[int]
+        :type B: List[int]
+        :rtype: List[int]
+        """
+        sortedA = sorted(A)
+        sortedB = sorted(B)
+
+        assigned = {b: [] for b in B}
+        remaining = []
+
+        j = 0
+        for a in sortedA:
+            if a > sortedB[j]:
+                assigned[sortedB[j]].append(a)
+                j += 1
+            else:
+                remaining.append(a)
+
+        return [assigned[b].pop() if assigned[b] else remaining.pop() for b in B]
