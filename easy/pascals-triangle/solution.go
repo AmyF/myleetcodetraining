b97f1d54df8ee30c115f36dd511b5ceb667e6322
unko <840382477@qq.com>
2018-11-03 23:18:03 +0800

    第六十九道

--- easy/pascals-triangle/solution.go
+++ easy/pascals-triangle/solution.go
@@ -1,15 +1,17 @@
 func generate(numRows int) [][]int {
-    if numRows == 0 {return [][]int{}}
-    ans := [][]int{}
-    ans = append(ans,[]int{1})
-    for i:=1;i<numRows;i++ {
-        tmp := make([]int,i+1)
-        tmp[0] = 1
-        for j:=1;j<i;j++ {
-            tmp[j] = ans[i-1][j-1] + ans[i-1][j]
-        }
-        tmp[i] = 1
-        ans = append(ans, tmp)
-    }
-    return ans
+	if numRows == 0 {
+		return [][]int{}
+	}
+	ans := [][]int{}
+	ans = append(ans, []int{1})
+	for i := 1; i < numRows; i++ {
+		tmp := make([]int, i+1)
+		tmp[0] = 1
+		for j := 1; j < i; j++ {
+			tmp[j] = ans[i-1][j-1] + ans[i-1][j]
+		}
+		tmp[i] = 1
+		ans = append(ans, tmp)
+	}
+	return ans
 }
